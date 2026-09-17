12b2099f7c3daa65de7d4fec13d1820c890d258f
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-17 22:30:41 +0800

    Basic negative number conversion

---
+++ JavaScript-Math-Method.js
@@ -0,0 +1,8 @@
+// 1. Fundamental Rounding & Absolute Value (Beginner)
+// Math.abs()
+// Returns the absolute (non-negative) value of a number.
+
+// Example 1: Basic negative number conversion
+const result1 = Math.abs(-10);
+console.log(result1);
+// Output: 10
