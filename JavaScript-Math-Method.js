e244167dcd0cc48da299b0ca2531786217006e6e
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-09-17 22:31:16 +0800

    Distance between two points

--- JavaScript-Math-Method.js
+++ JavaScript-Math-Method.js
@@ -6,3 +6,10 @@
 const result1 = Math.abs(-10);
 console.log(result1);
 // Output: 10
+
+// Example 2: Distance between two points
+const pos1 = 15;
+const pos2 = 42;
+const distance = Math.abs(pos1 - pos2);
+console.log(distance);
+// Output: 27
